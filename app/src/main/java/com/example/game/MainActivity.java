6e6d845070cad2e59eca7bd1bee3547af120a153
moha-b <20193015@fue.edu.eg>
2022-06-04 11:54:18 +0200

    open GL setup

--- app/src/main/java/com/example/game/MainActivity.java
+++ app/src/main/java/com/example/game/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.game;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        GLSurfaceView openGlView;
+        OpenGlRenderer glRenderer = new OpenGlRenderer();
+
+        openGlView = findViewById(R.id.openGl);
+        openGlView.setRenderer(glRenderer);
+
     }
+
 }
